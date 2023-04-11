04109cd13e57f1d1c8d5195c0cbc4bc6765074fd
Hector Agudelo <haacny86@gmail.com>
2023-04-11 16:52:06 -0400

    adding socket feature to communicate front and backend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import NavBar from "../components/navBar/NavBar";
-import ChatRoomSidebar from "../components/ChatRoomSidebar/ChatRoomSidebar";
-import { MessageOutput } from "../components/MessageOutput/MessageOutput";
-import { MessageInputDisplay } from "../components/MessageInputDisplay/MessageInputDisplay";
-import io from "socket.io-client";
-import { useAuth } from "../utils/authProvider";
-const socket = io.connect("http://localhost:800");
+import React, { useState, useEffect } from 'react';
+import NavBar from '../components/navBar/NavBar';
+import ChatRoomSidebar from '../components/ChatRoomSidebar/ChatRoomSidebar';
+import { MessageOutput } from '../components/MessageOutput/MessageOutput';
+import { MessageInputDisplay } from '../components/MessageInputDisplay/MessageInputDisplay';
+import io from 'socket.io-client';
+import { useAuth } from '../utils/authProvider';
+const socket = io.connect('http://localhost:800');
 
 export const Home = () => {
   const [isOpenRooms, setIsOpenRooms] = useState(true);
@@ -15,32 +15,39 @@ export const Home = () => {
     setIsOpenRooms(!isOpenRooms);
   };
 
-  const [inputMsg, setInputMsg] = useState("");
-
-  const sendMessage = () => {
-    // socket.emit()
-  };
+  const [inputMsg, setInputMsg] = useState('');
+  const [messageReceived, setMessageReceived] = useState('');
 
   const handleSubmit = () => {
     let newItem = {
-      name: auth.user,
+      name: (socket.id = auth.user),
       text: inputMsg,
       time: Date.now(),
     };
 
+    socket.emit('send_message', { newItem });
     setMsgList((msgList) => [...msgList, newItem]);
+  
   };
+
+  //create another object for the data received and send it to the bubble element to create the other users message
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setMessageReceived(data.message);
+      console.log(data)
+    });
+  }, [socket]);
 
   return (
     <>
       <NavBar openRooms={openRooms} />
-      <div className={`${isOpenRooms ? "flex" : " "}`}>
+      <div className={`${isOpenRooms ? 'flex' : ' '}`}>
         {isOpenRooms ? (
           <ChatRoomSidebar OpenRooms={openRooms} isOpenRooms={isOpenRooms} />
         ) : (
-          ""
+          ''
         )}
-        <div className="flex flex-col h-h-nav w-screen">
+        <div className='flex flex-col h-h-nav w-screen'>
           <MessageOutput
             user={auth.user}
             fullView={isOpenRooms}
