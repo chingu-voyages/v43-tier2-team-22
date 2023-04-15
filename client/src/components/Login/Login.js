67b49da80f2bd1280f7298c0ed87debeccc2b1ac
Alekzandriia <alekzoreillyz@me.com>
2023-04-15 10:44:30 -0400

    Added min-width to fix issue, add icons & colour
    Also added min-width to left sidebar where I noticed the same problem.
    
    & after reviewing the design file in Figma I realized I needed to add some icons to the sidebar.
    
    Added dark theme colours to messageInput

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -26,7 +26,7 @@ const Login = () => {
         <h1 className=" m-6 flex justify-center items-center text-4xl font-bold text-center">
           TechChat
         </h1>
-        <div className="w-1/4 p-6 m-auto rounded-3xl shadow-md lg:max-w-xl bg-gray-200">
+        <div className="min-w-fit w-1/4 p-6 m-auto rounded-3xl shadow-md lg:max-w-xl bg-gray-200">
           <h1 className="text-3xl font-semibold text-center text-purple-700">
             Login
           </h1>
@@ -36,7 +36,7 @@ const Login = () => {
                 placeholder="Type Nickname"
                 type="text"
                 name="username"
-                className="block w-3/4 px-4 py-2 mt-2 text-purple-700 bg-white border rounded-2xl focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40 text-center"
+                className=" block w-3/4 px-4 py-2 mt-2 text-purple-700 bg-white border rounded-2xl focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40 text-center"
               />
             </div>
 
