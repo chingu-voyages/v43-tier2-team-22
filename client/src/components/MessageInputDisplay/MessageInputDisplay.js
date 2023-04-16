e1ac05b12deae4bc643fabd16ce3b51cea475d1b
Hector Agudelo <haacny86@gmail.com>
2023-04-16 13:44:24 -0400

    messages among users working

--- client/src/components/MessageInputDisplay/MessageInputDisplay.js
+++ client/src/components/MessageInputDisplay/MessageInputDisplay.js
@@ -1,26 +1,21 @@
-import React, { useState } from "react";
-import { ImMail } from "react-icons/im";
+import React, { useState } from 'react';
+import { ImMail } from 'react-icons/im';
 
-export const MessageInputDisplay = ({
-  setInputMsg,
-  inputMsg,
-  fullView,
-  handleSubmit,
-}) => {
+export const MessageInputDisplay = ({ setInputMsg, handleSubmit }) => {
   return (
     <form
-      className="flex flex-row space-y-4 p-3  
+      className='flex flex-row space-y-4 p-3  
     scrollbar-thumb-blue scrollbar-thumb-rounded 
-    scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch bg-gray-200"
+    scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch bg-gray-200'
     >
       <input
-        type="text"
-        placeholder="Type your message here..."
-        className="border border-gray-400 p-2 rounded-md w-full mr-2 text-black focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+        type='text'
+        placeholder='Type your message here...'
+        className='border border-gray-400 p-2 rounded-md w-full mr-2 text-black focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
         onChange={(event) => setInputMsg(event.target.value)}
       />
       <button
-        className="!m-0 justify-center items-center flex"
+        className='!m-0 justify-center items-center flex'
         onClick={handleSubmit}
       >
         <ImMail size={40} />
